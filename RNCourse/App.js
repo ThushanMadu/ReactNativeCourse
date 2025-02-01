2f5d0bcf366ca9e47d59fc4d5e78fa37bd9612f3
ThushanMadu <thushanmadu2003@gmail.com>
2025-02-01 18:43:32 +0530

    change

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     },
     textInputContainer: {
         borderWidth:1,
-        borderColor:'#cccccc',
+        borderColor:'red',
         width:'80%',
     }
 
